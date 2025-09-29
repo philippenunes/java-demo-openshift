4133fd859d2e66edd0b17f04e7888402a2d514cd
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-09-29 18:43:41 -0300

    fix: trigger CI/CD and update message (v2) - tests fixed

--- src/main/java/com/example/demo/Home.java
+++ src/main/java/com/example/demo/Home.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 class Home {
     @GetMapping("/")
     public String home() {
-        return "GitOps LIVE: Java 17 + GitHub Actions + ArgoCD + OpenShift! 🚀";
+        return "GitOps LIVE: Java 17 + GitHub Actions + ArgoCD + OpenShift! 🚀 (v2)";
     }
 }
