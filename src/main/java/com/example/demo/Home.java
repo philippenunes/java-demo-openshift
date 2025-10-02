1682d82a9c668af353153c53324ff40d7aaba36e
Philippe Nunes <philippe.silva@mprj.mp.br>
2025-10-01 21:16:49 -0300

    including log

--- src/main/java/com/example/demo/Home.java
+++ src/main/java/com/example/demo/Home.java
@@ -5,8 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class Home {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(Home.class);
+
     @GetMapping("/")
     public String home() {
+        log.info("Java 17 + GitHub Actions + ArgoCD + OpenShift!");
         return "Java 17 + GitHub Actions + ArgoCD + OpenShift!";
     }
 }
